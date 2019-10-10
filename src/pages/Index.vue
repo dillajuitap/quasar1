e3fc46ac038baed5cd867dbe295cd2c6b8a2f44b
dillajuitap <dillajuitaputri@gmail.com>
2019-10-10 11:03:59 +0700

    coba

---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+  <button @click="message=''">clear </button>
+  <input v-model="message" @keyup="handleKeyup"/>
+    <H5> {{message}}</H5>
+  </q-page>
+</template>
+
+<script>
+export default {
+data() {
+return {
+  message: 'Test Vue'
+}
+},
+methods: {
+  clearMessage(){ 
+    this.message =''
+},
+  handleKeyup(e){
+    if(e.keyCode==27){
+      this.message='ESC'
+  } else if(e.keyCode==13){
+      this.message='ENTER'
+  }
+}
+}
+}
+</script>
